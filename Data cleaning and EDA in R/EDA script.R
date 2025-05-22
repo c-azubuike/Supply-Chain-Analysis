244e660eb0f376272e70da79daec777523c9dc04
c-azuby <c-azuby@github.com>
2025-05-21 23:04:53 -0700

    updates

--- Data cleaning and EDA in R/EDA script.R
+++ Data cleaning and EDA in R/EDA script.R
@@ -1,8 +1,12 @@
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(lubridate)
+library(stats)
+library(DescTools)
 
 df <- read_csv("df_cleaned.csv")
+
 glimpse(df)
 
 ## convert chars to factor
@@ -14,15 +18,17 @@ df <- df %>%
 #######################
 
 ## Which 3 countries are most orders shipped to ?
-top3_countries <- df %>% group_by(order_country) %>% 
-  summarize(order_count = n_distinct(order_id)) %>% 
+top3_countries <- df %>% count(order_country,name="order_count") %>% 
   arrange(desc(order_count)) %>% head(3)
+top3_countries
+
 
 ## Most ordered product in each of the top 3 destination countries
 most_ord_prods <- df %>% 
   filter(order_country %in% top3_countries$order_country) %>% 
   group_by(order_country) %>% count(product_name, sort=TRUE) %>% 
-  slice_max(n,n=1) %>% ungroup() #using ungroup to return the grouped object to a tibble
+  slice_max(n,n=1)
+most_ord_prods
   
 ## How does payment type vary across customer segment ?
 table(df$type) # since the level "Payment" is unclear, i will replace it with "Unknown"
@@ -33,13 +39,15 @@ top_payment_type <- df %>%
   group_by(customer_segment,type) %>% 
   summarize(count_in_each_segment = n(),.groups = "drop_last") %>% 
   mutate(percent_used = (count_in_each_segment/sum(count_in_each_segment)) * 100) %>% 
-  arrange(customer_segment,desc(percent_used)) %>% slice_max(percent_used,n=1) 
+  arrange(customer_segment,desc(percent_used)) %>% slice_max(percent_used,n=1)
+top_payment_type
 
 ## which shipping mode has the highest last delivery risk ?
 late_risk <- df %>% filter(late_delivery_risk == 1) %>% 
   count(shipping_mode,name = "count") %>% 
   mutate(total_sum = sum(count),percent_risk = (count/total_sum) * 100 ) %>% 
   arrange(desc(percent_risk))
+late_risk
   
 ## How do sales profit vary by order region and market ?
 profit_summary <- df %>% group_by(market,order_region) %>% 
@@ -50,14 +58,83 @@ profit_summary <- df %>% group_by(market,order_region) %>%
          percent_profit = (total_profit/market_profit) * 100,
          rank_in_region = rank(desc(total_profit))) %>% 
   arrange(market,rank_in_region)
+profit_summary
+
+
+##Are there seasonal patterns in sales when examining the order dates ?
+df$order_year <- as.factor(year(df$order_date_date_orders))
+df$order_month <- as.factor(month(df$order_date_date_orders, label = TRUE))
+
+yearly_sales <- df %>% filter(!is.na(order_year)) %>% 
+  group_by(order_year,order_month) %>% 
+  summarize(Total_sales = sum(sales)) %>% 
+  arrange(order_year,Total_sales)
+
+top_sales_per_Yr <- yearly_sales %>% slice_max(Total_sales,n=1) %>% 
+  mutate(Total_sales = scales::dollar(Total_sales))
+top_sales_per_Yr
+
+
+## Trend in late delivery by day of week
+df$weekday <- wday(df$shipping_date_date_orders,label = TRUE)
+df$shipping_delay <- df$days_for_shipping_real - df$days_for_shipment_scheduled
+df$shipped_late <- ifelse(df$shipping_delay > 0,1,0)
+df$delivered_late <- ifelse(df$delivery_status == "Late Delivery",1,0)
+df$shipped_on_time <- ifelse(df$delivery_status == "Shipping On Time",1,0)
+
+
+late_delivery_analysis <- df %>% group_by(weekday) %>% 
+  summarize(total_orders = n(),
+            shipped_late = sum(shipped_late, na.rm=TRUE),
+            shipped_on_time = sum(shipped_on_time,na.rm=TRUE),
+            late_delivery = sum(delivered_late,na.rm = TRUE),
+            late_rate = mean(delivered_late, na.rm=TRUE),
+            avg_shipping_delay = mean(shipping_delay[shipping_delay > 0],na.rm=TRUE)) %>% 
+  arrange(desc(late_rate))
+late_delivery_analysis
+            
+
+## Suspected fraud orders by order_region and country
+fraud_orders <- df %>% filter(order_status == "Suspected_fraud") %>% 
+  count(order_region,order_country,name="Suspected_fraud_count") %>% 
+  arrange(desc(Suspected_fraud_count))
+
+
+##############################
+##     STATISTICAL TESTS    ##
+##############################
+
+## Do benefit_per_order (a.k.a profits) vary significantly across market?
+test_1 <- summary(aov(benefit_per_order ~ market, data = df))
+#--since p-value is < 0.05,difference in profit across markets is significant
+
+
+## Is payment type dependent on customer segment ?
+test_2 <- chisq.test(table(df$type, df$customer_segment))
+
+test_2$expected # values are > 5 so test is reliable
+# pull the p-value
+test_2$p.value
+# -- since p-value is 0.0014, payment type is highly dependent on customer segment
+
+# check the strength of the association
+CramerV(table(df$type,df$customer_segment)) # 0.009862739
+# even though the association is statistically significant, it is extremely weak in practical terms.
 
 
 
 
 
+## is late delivery dependent of day of week ?
+test_3 <- chisq.test(table(df$weekday,df$delivered_late))
 
+test_3$expected # values are all greated than 5
 
-
-
+# -- most late deliveries happen on Saturdays, and since p-value = 9.44e-05
+# this is statistically significant, and association with late delivery is likely
+# not due to chance
+CramerV(table(df$weekday, df$delivered_late)) # 0.01587152
+# extremely weak practical association even though Saturdays have the highest 
+# late delivery rate, the overall impact is minimal.
 
 
--- Data cleaning and EDA in R/data cleaning in R.R
+++ Data cleaning and EDA in R/data cleaning in R.R
@@ -2,6 +2,9 @@ library(dplyr)
 library(readr)
 library(janitor)
 library(stringr)
+library(hunspell)
+library(corrplot)
+
 
 
 ## read the data
@@ -39,7 +42,7 @@ sum(is.na(df)) # total number of missing values in the entire data set
 colSums(is.na(df)) # missing values per column
 
 #######
-## drop columns with high number of missing values
+## drop unimportant columns with high number of missing values
 #######
 df <- df %>% select(-c(product_description,order_zipcode))
 # find the other columns with missing values
@@ -52,23 +55,28 @@ df[is.na(df$customer_lname),]
 df %>% filter(is.na(customer_lname)) %>% select(customer_fname,customer_lname)
 df[is.na(df$customer_zipcode),]
 
+# since removing these rows will not affect the data set, they will be dropped
+
 #######
 ## drop the 9 rows (from customer_lname and zipcode) with missing values
 #######
 df[!complete.cases(df),]
 df <- df[complete.cases(df),]
 
+sum(is.na(df))
+
 #######
-## check for duplicate rows/columns
+## check for duplicate rows
 #######
 df[duplicated(df),] # rows
 
 #######
-## verify that date columns contain dates
+## verify/set that date columns to data datatype
 #######
 df %>% select(contains("date"))
 # convert cols to datetime
 df <- df %>% mutate(across(contains("date"),~ as.POSIXct(.,format="%m/%d/%Y %H:%M")))
+df %>% select(contains("date"))
 
 #######
 ## select char cols and check for empty strings and extra white space
@@ -77,7 +85,7 @@ char_cols <- df %>%
   select(where(is.character)) # select all the character cols
 
 ## replace empty strings with NA
-chars_cols <- char_cols %>% 
+char_cols <- char_cols %>% 
   mutate(across(everything(),~na_if(.,"")))
 
 ## trim unnecessary white spaces
@@ -87,10 +95,42 @@ unique(char_cols$order_country)
 
 ## change encoding
 char_cols <- char_cols %>% 
-  mutate(across(everything(),~iconv(.x,from="Latin1",to="UTF-8")))
-  
+  mutate(across(everything(),~iconv(.,from="Latin1",to="UTF-8")))
+
+unique(char_cols$order_city) 
+unique(char_cols$order_country)
 # trim white space
 char_cols <- char_cols %>% mutate(across(everything(), ~str_trim(.)))
+
+
+#######
+## check for misspelled words
+#######
+misspelled_df <- char_cols %>% mutate(across(everything(),~hunspell_find(.)))
+
+# since each cell of "misspelled_df" is a list, count each cell to get the number
+# of misspelled words
+misspelled_lengths <- misspelled_df %>% 
+  mutate(across(everything(),~lengths(.)))
+
+misspelled_summary <- misspelled_lengths %>% 
+  summarize(across(everything(),sum))
+
+## verify misspelled word in each flagged column
+cat_name_mask <- which((lengths(misspelled_df$category_name) > 0))
+table(as.factor(char_cols$category_name[cat_name_mask])) 
+# category_name contains words with special characters, but are not misspelled
+
+# customer_city
+cust_city <- char_cols$customer_city[which(lengths(misspelled_df$customer_city) > 0)]
+unique(as.factor(cust_city)) %>% sort()
+
+# since customer city,country can contain special characters like accent markrs
+# both columns will not be evaluated
+
+# similarly, since customer first_name, last_name, email and password can contain
+# special character and have different variations, these columns will be left as is
+
 
 #######
 ## drop char columns that have no variability/ high cardinality
@@ -101,16 +141,20 @@ char_col_names <- names(char_cols)
 sort(char_col_var,decreasing = FALSE)
 var_cols <- char_col_names[char_col_var <=1 | char_col_var > 200]
 char_cols <- char_cols %>% select(-all_of(var_cols))
-char_cols <- char_cols %>% select(-product_image) # unimportant column
+names(char_cols) # verify
 
+char_cols <- char_cols %>% select(-product_image) # unimportant column
+names(char_cols) # verify
+head(char_cols)
 char_cols$customer_state <- str_to_upper(char_cols$customer_state)
 
 #######
 ## convert char cols to factor
 #######
 char_cols <- char_cols %>% mutate(across(everything(), ~ as.factor(.)))
-
+head(char_cols)
 str(char_cols)
+
 
 
 
@@ -118,26 +162,54 @@ str(char_cols)
 ## clean numerical cols
 #######
 num_cols <- df %>% select(where(is.numeric))
-num_cols
 
+## check cardinality and drop cols with no variability
+sapply(num_cols,n_distinct)
+num_cols <- num_cols %>% select(-product_status)
+
+#######
 ## check for duplicate columns
+#######
+
+## build a correlation matrix
+corr_matrix <- cor(num_cols)
+corrplot(corr_matrix,method="color",addCoef.col = "black",number.cex = 0.5)
+
+diag(corr_matrix) <- NA # set diagonal to NA to remove correlation with self
+which(abs(corr_matrix) == 1,arr.ind = TRUE)
+
+# verify result using duplicated
 dupes <- duplicated(t(num_cols))
 num_cols[,dupes]
 
-## remove duplicates
-dupe_num_cols <- names(num_cols[,dupes])
-num_cols <- num_cols %>% select(-all_of(dupe_num_cols))
+## drop duplicated columns/remove id columns
+num_cols <- num_cols[,!dupes]
 
-str(num_cols)              
+id_columns <- num_cols %>% select(contains("_id"))
+num_cols <- num_cols %>% select(-names(id_columns))
+names(num_cols)
+
 
 
 #######
-## combine the cleaned columns
+## combine the cleaned columns with the datetime column
 #######
 other_cols <- df %>% select(-where(is.character), -where(is.numeric))
 
-df_cleaned <- bind_cols(char_cols,num_cols,other_cols)
+
+df_combined <- bind_cols(char_cols,num_cols,other_cols)
 
 
-write_csv(df_cleaned,"df_cleaned.csv")
+#######
+## Check for NA values introduced by the datetime columns
+#######
+sum(is.na(df_combined))
+colSums(is.na(df_combined))
+
+# drop the NA values 
+df_combined <- df_combined[complete.cases(df_combined),]
+sum(is.na(df_combined)) # verify changes
+
+
+write_csv(df_combined,"df_cleaned.csv")
 
